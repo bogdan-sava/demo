f43d305fdb1305765228692175e2e3faaa3d2c7d
Diana Cozac <platona.diana@gmail.com>
2023-02-16 19:56:44 +0200

    created the date model class "Employees".

---
+++ src/main/java/com/example/demo/model/Employees.java
@@ -0,0 +1,124 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+public class Employees {
+
+    private String fullName;
+    private String jobTitle;
+    private String department;
+    private String businessUnit;
+    private String gender;
+    private String ethnicity;
+    private int age;
+    private Date hireDate;
+    private float annualSalary;
+    private int bonus;
+    private String country;
+    private String city;
+    private Date exitDate;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getBusinessUnit() {
+        return businessUnit;
+    }
+
+    public void setBusinessUnit(String businessUnit) {
+        this.businessUnit = businessUnit;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEthnicity() {
+        return ethnicity;
+    }
+
+    public void setEthnicity(String ethnicity) {
+        this.ethnicity = ethnicity;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public float getAnnualSalary() {
+        return annualSalary;
+    }
+
+    public void setAnnualSalary(float annualSalary) {
+        this.annualSalary = annualSalary;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(int bonus) {
+        this.bonus = bonus;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Date getExitDate() {
+        return exitDate;
+    }
+
+    public void setExitDate(Date exitDate) {
+        this.exitDate = exitDate;
+    }
+}
